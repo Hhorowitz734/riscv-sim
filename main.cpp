a6caeb1151642d7cccd3542989d0497319f598f0
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-09-23 11:23:56 -0500

    Updated code to take params

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 #include "include/lexer.h"
 
 int main(int argc, char* argv[]) { 
+
+    // Good command to run this: ./riscv-sim ../test.txt Hi Hi
+
+
+    // Not enough params
+    if (argc < 3) {
+        std::cerr << "Please pass all required parameters: \n      --Inputfilename \n      --Outputfilename \n      --Operation" << std::endl;
+        exit(1);
+    } 
+
+    // Process params
+    std::string inputfile = argv[1];
+    std::string outputfile = argv[2];
+    std::string operation = argv[3];
+
+    // Print params for testing
+    std::cout << "Parameters passed: \n" << "Input filename: " << inputfile << "\nOutput filename: " << outputfile << "\nOperation: " << operation << std::endl; 
     
     Lexer* lexer = new Lexer();
 
-    std::string filename = "../test.txt";
 
-    lexer->open_file(const_cast<char*>(filename.c_str()));
+    lexer->open_file(const_cast<char*>(inputfile.c_str()));
 
 
     std::cout << "File size: " << static_cast<int>(lexer->getFileSize()) << std::endl;
